3aa9c06305211ff6d738e4b6c572c7d7aae439fa
hikmatulloh <101179120+tuloAminov@users.noreply.github.com>
2022-12-18 19:11:21 +0500

    Update AuthorRepository.java

--- homework5/MyBookShopApp/src/main/java/com/example/MyBookShopApp/data/AuthorRepository.java
+++ homework5/MyBookShopApp/src/main/java/com/example/MyBookShopApp/data/AuthorRepository.java
@@ -2,5 +2,7 @@ package com.example.MyBookShopApp.data;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+@Repository
 public interface AuthorRepository extends JpaRepository<Author, Integer> {
+    Author findAuthorById(Long id);
 }
