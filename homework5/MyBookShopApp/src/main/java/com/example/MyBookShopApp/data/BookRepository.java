60217376c78e093216b76bc1246e08c5251a81e5
hikmatulloh <101179120+tuloAminov@users.noreply.github.com>
2022-12-18 19:11:59 +0500

    Update BookRepository.java

--- homework5/MyBookShopApp/src/main/java/com/example/MyBookShopApp/data/BookRepository.java
+++ homework5/MyBookShopApp/src/main/java/com/example/MyBookShopApp/data/BookRepository.java
@@ -1,14 +1,15 @@
 package com.example.MyBookShopApp.data;
 
+import com.example.mybookshopapp.data.Book;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface BookRepository extends JpaRepository<Book, Integer> {
-
-    List<Book> findBooksByAuthor_FirstName(String name);
-
     @Query("from Book")
     List<Book> customFindAllBooks();
 }
